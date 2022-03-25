63798a7429bb122b37365f78e2ad123384b581fb
ggyul <wakegyul@gmail.com>
2022-03-25 23:57:41 +0900

    [Alprac] Solve 448C - Divide and conquer + Greedy? - professor
    while로 0이 아닌 부분만 체크하는 게 더 깔끔한 것 같다.

--- alprac-submissions/divide-conquer/main.cpp
+++ alprac-submissions/divide-conquer/main.cpp
@@ -4,41 +4,39 @@ using namespace std;
 // * https://codeforces.com/problemset/problem/448/C
 
 int n;
-int arr[5001];
+int input[5001];
 
-int paintFence(int start, int end) {
-    if (start == end) return 0;
-    // 최솟값 찾기
+int paintFence(int arr[], int s) {
     int minHeight = 1'000'000'001;
-    for (int i = start; i < end; i++) {
+    for (int i = 0; i < s; i++) {
         minHeight = min(arr[i], minHeight);
     }
     // 나머지 부분에 대해 재귀적으로 계산하기 위해, 칠한 부분 뺌
-    for (int i = start; i < end; i++) {
+    for (int i = 0; i < s; i++) {
         arr[i] -= minHeight;
     }
+    int result = minHeight;
+
     // 이제 돌면서 남은 부분 탐색
-    int s = start - 1;
-    int result = 0;
-    for (int i = start; i < end; i++) {
-        if (arr[i] == 0) {
-            result += paintFence(s + 1, i);
-            s = i;
-        }
+    int p = 0;
+    while (p < s) {
+        while (p < s && arr[p] == 0) p++;
+        // * 없을 경우!
+        if (p == s) break;
+        int q = p;
+        while (q + 1 < s && arr[q + 1] != 0)q++;
+        result += paintFence(arr + p, q - p + 1);
     }
-    // 마지막도 확인
-    result += paintFence(s + 1, end);
     // 근데 가로로 칠하는 것보다 그냥 세로로 칠하는 게 더 적은 경우일 수 있다.
     // 따라서 비교해서 더 작은 걸로
-    result = min(end - start, result + minHeight);
-    return result;
+    return min(result, s);
 }
 
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
-        cin >> arr[i];
+        cin >> input[i];
     }
-    cout << paintFence(0, n) << endl;
+    cout << paintFence(input, n) << endl;
     return 0;
 }
